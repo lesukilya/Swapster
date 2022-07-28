5aaa780d6044b599467839a7edd7205089bb9da3
lesukilya <lesuk5577@gmail.com>
2022-07-28 23:02:19 +0300

    3page

--- index.php
+++ index.php
@@ -21,6 +21,111 @@ require( 'inc/header.php' );
             <img src="/assets/img/userfiles/firstScreen-phone.png" alt="" class="b-firstScreen__img">
         </div>
     </section>
+    <section class="l-secondScreen">
+        <div class="b-secondScreen">
+            <img src="/assets/img/userfiles/secondScreen-phone2.png" alt="" class="b-secondScreen__img">
+            <div class="b-secondScreen__side">
+                <h2 class="b-secondScreen__title">
+                Самое важное
+                </h2>
+                <div class="b-secondScreen__article">
+                    <div class="b-secondScreen__articleVidgetWrapper">
+                        <img src="/assets/img/userfiles/secondScreen-vidget1.svg" alt="" class="b-secondScreen__articleVidget">
+                    </div>
+                    <div class="b-secondScreen__articleInfo">
+                        <h3 class="b-secondScreen__articleTitle">
+                            Бесплатные транзакции
+                        </h3>
+                        <p class="b-secondScreen__articleText miniText">
+                            Транзакции другим пользователям Swapster абсолютно бесплатны. Вы можете одним кликом отправить другу любую криптовалюту за секунду — достаточно знать его ID.
+                        </p>
+                    </div>
+                </div>
+                <div class="b-secondScreen__article">
+                    <div class="b-secondScreen__articleVidgetWrapper">
+                        <img src="/assets/img/userfiles/translate.svg" alt="" class="b-secondScreen__articleVidget">
+                    </div>
+                    <div class="b-secondScreen__articleInfo">
+                        <h3 class="b-secondScreen__articleTitle">
+                            Почти моментальные переводы
+                        </h3>
+                        <p class="b-secondScreen__articleText">
+                            В комиссиях Swapster уже заложено щедрое вознаграждение майнерам, поэтому все ваши переводы будут доставлены максимально быстро. USDT — 1 минута. ВТС, ЕТН — 10 минут.                        
+                        </p>
+                    </div>
+                </div>
+                <div class="b-secondScreen__article">
+                    <div class="b-secondScreen__articleVidgetWrapper">
+                        <img src="/assets/img/userfiles/secondScreen-vidget3.svg" alt="" class="b-secondScreen__articleVidget">
+                    </div>
+                    <div class="b-secondScreen__articleInfo">
+                        <h3 class="b-secondScreen__articleTitle">
+                            Анонимность и безопасность
+                        </h3>
+                        <p class="b-secondScreen__articleText">
+                            Мы не собираем и не передаём персональные данные пользователей. Вам доступны все функции бота без верификации личности. Безопасность ваших средств обеспечена безопасностью вашего Telegram-аккаунта.                        
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="l-contentBenefits">
+        <div class="b-contentBenefits">
+            <h2 class="b-contentBenefits__titleFirst">
+                Еще преимущества
+            </h2>
+            <div class="b-contentBenefits__cardsHorizontal">
+                <div class="b-contentBenefits__card">
+                    <img src="/assets/img/userfiles/contentBenedfits-vector1.svg" alt="" class="b-contentBenefits__img b-contentBenefitsImg1">
+                    <h3 class="b-contentBenefits__title">
+                        Автоматические выплаты на любые счета российских банков
+                    </h3>    
+                    <p class="b-contentBenefits__text">
+                        Пользуясь криптокошельком в ТГ, вам не требуется ждать, когда придут деньги, как это происходит в обменниках и биржах. Выплаты переводятся мгновенно. Для комфорта можно сохранить список рабочих карт, чтобы не вводить номера при каждом переводе.
+                    </p>   
+                </div>
+                <div class="b-contentBenefits__card">
+                    <img src="/assets/img/userfiles/contentBenedfits-vector2.svg" alt="" class="b-contentBenefits__img b-contentBenefitsImg2">
+                    <h3 class="b-contentBenefits__title">
+                        Выгодный обмен криптовалют без больших комиссий                    
+                    </h3>    
+                    <p class="b-contentBenefits__text">
+                        При конвертации криптовалют не нужно оплачивать дополнительную комиссию. Курс фиксируется каждые 5 секунд и обновляется в режиме онлайн.                    
+                    </p>   
+                </div>
+                <div class="b-contentBenefits__card">
+                    <img src="/assets/img/userfiles/contentBenedfits-vector3.svg" alt="" class="b-contentBenefits__img b-contentBenefitsImg3">
+                    <h3 class="b-contentBenefits__title b-contentBenefits-title3">
+                        Общий баланс USDT для всех сетей без комиссии                    
+                    </h3>    
+                    <p class="b-contentBenefits__text">
+                        Переводите USDT по сетям TRC20, ERC20, BEP20 с одного криптокошелька в ТГ. Получили перевод по одной сети? Можете отправить его по другой. Перекидывать средства между счетами не нужно.                    
+                    </p>   
+                </div>
+            </div> 
+            <div class="b-contentBenefits__cardsVertical">
+                <div class="b-contentBenefits__cardVertical">
+                    <img src="/assets/img/userfiles/contentBenedfits-vector4.svg" alt="" class="b-contentBenefits__img b-contentBenefitsImg4">
+                    <h3 class="b-contentBenefits__title">
+                        Поддержка 24/7
+                    </h3>    
+                    <p class="b-contentBenefits__text b-contentBenefits-text4">
+                        Сотрудники находятся на связи круглосуточно. Можете обратиться с появившимися вопросами и предложениями о работе криптокошелька в ТГ в любое время.
+                    </p>   
+                </div>
+                <div class="b-contentBenefits__cardVertical">
+                    <img src="/assets/img/userfiles/contentBenedfits-vector5.svg" alt="" class="b-contentBenefits__img b-contentBenefitsImg5">
+                    <h3 class="b-contentBenefits__title b-contentBenefits-title5">
+                        Поддержка 24/7
+                    </h3>    
+                    <p class="b-contentBenefits__text b-contentBenefits-text5">
+                        Сотрудники находятся на связи круглосуточно. Можете обратиться с появившимися вопросами и предложениями о работе криптокошелька в ТГ в любое время.
+                    </p>   
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php require( 'inc/footer.php' ); ?>
