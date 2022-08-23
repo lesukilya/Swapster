66e4283379816fd27476e4ae7e39698fb4a6971a
lesukilya <lesuk5577@gmail.com>
2022-08-23 22:45:38 +0300

    fuck

--- how.php
+++ how.php
@@ -19,7 +19,6 @@ require( 'inc/header.php' );
             </div>
             <img src="/assets/img/userfiles/phoneHow.png" alt="" class="b-firstScreen__img -how">
             <img src="/assets/img/userfiles/moneyHow.png" alt="" class="b-firstScreen__imgMonets">
-            <img src="/assets/img/userfiles/monetsPhone.png" alt="" class="b-firstScreen__imgPhone">
         </div>
         <div class="b-firstScreen__stroke">
         </div>
@@ -40,6 +39,7 @@ require( 'inc/header.php' );
                             Установите ТГ аккаунт
                         </p>
                     </div>
+                    <img src="/assets/img/userfiles/arrowHow.png" alt="" class="b-contentStep__imgArrow">
                 </div>
                 <div class="b-contentStep__card -two">
                     <img src="/assets/img/userfiles/step2How.png" alt=""  class="b-contentStep__img">
@@ -51,6 +51,7 @@ require( 'inc/header.php' );
                             Активируйте криптокошелек по ссылке
                         </p>
                     </div>
+                    <img src="/assets/img/userfiles/arrowHow.png" alt="" class="b-contentStep__imgArrow">
                 </div>
                 <div class="b-contentStep__card -three">
                     <img src="/assets/img/userfiles/step2How.png" alt=""  class="b-contentStep__img">
@@ -120,6 +121,161 @@ require( 'inc/header.php' );
             <p class="b-contentReplenishment__endText">
                 *В скором времени появиться возможность пополнить свой кошелек и при помощи банковской карты.
             </p>
+        </div>
+    </section>
+    <section  class="l-contentWithdrawals">
+        <div class="b-contentWithdrawals">
+            <h2 class="b-contentWithdrawals__title">Выводы криптовалют и фиатных средств</h2>
+            <p  class="b-contentWithdrawals__text">
+                Для того, чтобы вывести криптовалюту или фиатные средства вам необходимо в основном меню выбрать раздел «Отправить», затем выбрать криптовалюту из выпадающего списка, выбрать нужный блокчейн, ввести адрес, куда вы хотите вывести средства и ввести сумму вывода
+            </p>
+            <ul class="b-contentWithdrawals__text -middle">
+                <li class="b-contentWithdrawals__textMiddle">
+                    Время вывода зависит от загруженности сети, но в среднем оно колеблется: 
+                </li>
+                <li class="b-contentWithdrawals__textMiddle">
+                    для сети Tron(USDT TRC20) от 1 до 3 секунд;
+                </li>
+                <li class="b-contentWithdrawals__textMiddle">
+                    для сети Binance Smart Chain (USDT BEP) от 1 до 2 секунд;
+                </li>
+                <li class="b-contentWithdrawals__textMiddle">
+                    для сети ETH (USDT ERC20) от 2 до 10 минут;
+                </li>
+                <li class="b-contentWithdrawals__textMiddle">
+                    для сети ВТС от 5 до 40 минут
+                </li>
+            </ul>
+            <p class="b-contentWithdrawals__text">
+                Вывод криптовалют на другие криптокошельки происходит аналогично пополнению — Вам необходимо выбрать раздел «Отправить» — затем выбрать валюту, которую Вы планируете вывести, указать адрес криптокошелька получателя и указать сумму выводимых активов. После этого подтвердить детали вывода и сумма будет немедленно выведена с вашего криптокошелька. Вы также получите квитанцию и ссылку на хеш транзакции.
+                <br> <br> Ниже представлена инструкция по продаже криптоактивов и прямому выводу на банковскую карту.
+            </p>
+            <div class="b-contentWithdrawals__cardsFirst">
+                <div class="b-contentWithdrawals__cardLine -one">
+                    <div class="b-contentWithdrawals__card -oneImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentWithdrawals__info">
+                            <p class="b-contentWithdrawals__infoTitle">01</p>
+                            <p class="b-contentWithdrawals__infoText">Выберите раздел Visa/MasterCard</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentWithdrawals__cardLine -two">
+                    <div class="b-contentWithdrawals__card -twoImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentWithdrawals__info">
+                            <p class="b-contentWithdrawals__infoTitle">02</p>
+                            <p class="b-contentWithdrawals__infoText">Выберите криптовалюту для вывода</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentWithdrawals__cardLine -three">
+                    <div class="b-contentWithdrawals__card -threeImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentWithdrawals__info">
+                            <p class="b-contentWithdrawals__infoTitle">03</p>
+                            <p class="b-contentWithdrawals__infoText">Введите номер карты, куда вы хотите получить средства</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="b-contentWithdrawals__cardsSecond">
+                <div class="b-contentWithdrawals__cardLine -fourth">
+                    <div class="b-contentWithdrawals__card -fourthImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentWithdrawals__info">
+                            <p class="b-contentWithdrawals__infoTitle">04</p>
+                            <p class="b-contentWithdrawals__infoText">Введите сумму, которую вы хотите обменять</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentWithdrawals__cardLine -fifth">
+                    <div class="b-contentWithdrawals__card -fifthImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentWithdrawals__info">
+                            <p class="b-contentWithdrawals__infoTitle">05</p>
+                            <p class="b-contentWithdrawals__infoText">Проверьте введенные данные и подтвердите вывод средств</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="l-contentTrades">
+        <div class="b-contentTrades">
+            <h2 class="b-contentTrades__title">Обмены и переводы</h2>
+            <p class="b-contentTrades__text">
+                Swapster — это не только мультивалютный кошелек, но и полноценная биржа внутри вашего Телеграм аккаунта. Поэтому обменивать криптовалюту между собой и на рубли вы можете буквально в два клика.
+                <br><br> Выберите основное меню своего кошелька, нажмите на раздел «Обменять», выберите криптовалюту, которую хотите обменять или рубли, выберите, что вы хотите получить взамен, затем выберите сумму продажи или сумму покупки желаемой валюты и введите сумму.
+            </p>
+            <div class="b-contentTrades__cards">
+                <div class="b-contentTrades__cardLine -one">
+                    <div class="b-contentTrades__card -oneImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentTrades__info">
+                            <p class="b-contentTrades__infoTitle">01</p>
+                            <p class="b-contentTrades__infoText">Выберите раздел «Обменять»</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentTrades__cardLine -two">
+                    <div class="b-contentTrades__card">
+                        <img src="/assets/img/userfiles/cardInfo2.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentTrades__info">
+                            <p class="b-contentTrades__infoTitle">02</p>
+                            <p class="b-contentTrades__infoText">Выберите криптовалюту для обмена</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentTrades__cardLine -three">
+                    <div class="b-contentTrades__card">
+                        <img src="/assets/img/userfiles/cardInfo2.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentTrades__info">
+                            <p class="b-contentTrades__infoTitle">03</p>
+                            <p class="b-contentTrades__infoText">Выберите криптовалюту, которую Вы хотите получить взамен</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentTrades__cardLine -fourth">
+                    <div class="b-contentTrades__card">
+                        <img src="/assets/img/userfiles/cardInfo4.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentTrades__info">
+                            <p class="b-contentTrades__infoTitle">04</p>
+                            <p class="b-contentTrades__infoText -big">Выберите что вам проще определить —сколько вы хотите продать или сколько вы хотите получить криптовалюты</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentTrades__cardLine -fifth">
+                    <div class="b-contentTrades__card -fifthImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentTrades__info">
+                            <p class="b-contentTrades__infoTitle">05</p>
+                            <p class="b-contentTrades__infoText">Выберите сумму криптовалюты</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="b-contentTrades__cardLine -sixth">
+                    <div class="b-contentTrades__card -sixthImg">
+                        <img src="/assets/img/userfiles/Replenishment1.png" alt="" class="b-contentWithdrawals__img">
+                        <div class="b-contentTrades__info">
+                            <p class="b-contentTrades__infoTitle">06</p>
+                            <p class="b-contentTrades__infoText">Проверьте введенные данные и подтвердите вывод средств</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="l-contentStart">
+        <div class="b-contentStart">
+            <div class="b-contentStart__wrapper">
+                <h2 class="b-contentStart__title">
+                    Запустить Swapster и создать все доступные кошельки в 1 клик
+                </h2>
+                <a href="#" class="b-wallet__link button hvr-grow">Запустить Swapster</a>
+                <img src="/assets/img/userfiles/startPhone.png" alt="" class="b-contentStart__img">
+                <img src="/assets/img/userfiles/графика2 1.png" alt="" class="b-contentStart__img1">
+            </div>
         </div>
     </section>
 </main>
